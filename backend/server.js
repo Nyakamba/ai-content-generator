86470e27178503304c67a60c8ef109ba36ea0f57
Nyakamba <omwegaenock@gmail.com>
2024-02-20 23:16:24 +0300

    User registration

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const usersRouter = require("./routes/usersRouter");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//-----Routes-----
+app.use("/api/v1/users", usersRouter);
 
 //start the server
 app.listen(PORT, () => {
