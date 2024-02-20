4362c7654d8a03cb2aa51bdb56f06433aa39f218
Nyakamba <omwegaenock@gmail.com>
2024-02-20 23:00:25 +0300

    users controller

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,14 @@
+// Registration
+const register = (req, res) => {
+  res,
+    json({
+      status: true,
+      message: "REgistration  was successiful",
+    });
+};
+//Login
+//Logout
+//Profile
+//Check user Auth Status
+
+module.exports = { register };
