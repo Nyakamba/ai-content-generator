419b46c21bb8fddbfd182e49311e3c93af05192c
Nyakamba <omwegaenock@gmail.com>
2024-02-21 12:42:16 +0300

    connecting to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const usersRouter = require("./routes/usersRouter");
+require("./utils/connectDB")();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//----middleware----
+app.use(express.json()); //pas incoming json data
 //-----Routes-----
 app.use("/api/v1/users", usersRouter);
 
