da4342175a64c6885a29166547785b2e212f9b0e
Nyakamba <omwegaenock@gmail.com>
2024-02-21 11:41:16 +0300

    registration of user logic

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,9 +1,50 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
 // Registration
-const register = (req, res) => {
-  res.json({
-    status: true,
-    message: "Registration  was successful",
-  });
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    //Validate
+    if (!username || !email || !password) {
+      res.status(400);
+      throw new Error("Please all fields are required");
+    }
+    //check the email if it is taken
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+    //hash passowrd
+    const salt = await bcrypt.genSalt(10);
+    const hashedPasword = await bcrypt.hash(password, salt);
+
+    //register user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPasword,
+    });
+    //Add the date the trial will end
+    newUser.trialExpires = new Date(
+      new Date().getTime() + newUser.trialPeriod * 24 * 60 * 60 * 1000
+    );
+    //save the user
+    await newUser.save();
+
+    res.json({
+      status: true,
+      message: "Registration  was successful",
+      user: {
+        username,
+        email,
+        trialExpires,
+      },
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 //Login
 //Logout
