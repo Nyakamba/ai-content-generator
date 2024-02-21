a1d8f314d672dfc3cf58df739e8c510c45ace3fe
Nyakamba <omwegaenock@gmail.com>
2024-02-21 16:45:52 +0300

    error handling

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+require("dotenv").config();
 const usersRouter = require("./routes/usersRouter");
+const { errorHandler } = require("./middlewares/errorMiddleware");
 require("./utils/connectDB")();
 
 const app = express();
@@ -10,6 +12,8 @@ app.use(express.json()); //pas incoming json data
 //-----Routes-----
 app.use("/api/v1/users", usersRouter);
 
+//--error handler middleware---
+app.use(errorHandler);
 //start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
