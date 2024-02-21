f513dd77ba96e5647804bbcc85ea0b77e397e124
Nyakamba <omwegaenock@gmail.com>
2024-02-21 21:21:27 +0300

    login controller

--- backend/utils/connectDB.js
+++ backend/utils/connectDB.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://omwegaenock:enock4501@ai-mern.noph0yj.mongodb.net/AI-MERN?retryWrites=true&w=majority"
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
     console.log(`Mongodb connected ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB ${error.message}`);
