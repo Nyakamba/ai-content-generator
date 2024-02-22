edaeb180a2c9f80abac0feab1d1c3313e691eba5
Nyakamba <omwegaenock@gmail.com>
2024-02-22 11:51:52 +0300

    sending requests to open ai

---
+++ backend/controllers/openAIController.js
@@ -0,0 +1,27 @@
+const asyncHandler = require("express-async-handler");
+const axios = require("axios");
+
+//OpenAI Controller
+
+const openAIController = asyncHandler(async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/completions",
+      {
+        model: "gpt-3.5-turbo-instruct",
+        prompt,
+        max_tokens: 10,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(response);
+  } catch (error) {}
+});
+
+module.exports = openAIController;
