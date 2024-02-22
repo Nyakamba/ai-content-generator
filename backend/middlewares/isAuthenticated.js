55831fde19c330e0f23d59816780605b91ba6ba8
Nyakamba <omwegaenock@gmail.com>
2024-02-22 11:07:47 +0300

    protecting routes

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,13 +1,15 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 //isAuthenticated middleware
 const isAuthenticated = asyncHandler(async (req, res, next) => {
   if (req.cookies.token) {
     //!verify token
-    const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
-
-    console.log(decoded);
+    const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET); //the actual login user
+    //add the user to the req obj
+    req.user = await User.findById(decoded?.id).select("-password");
+    return next();
   } else {
     return res.status(401).json({ message: "Not authorized, no token" });
   }
