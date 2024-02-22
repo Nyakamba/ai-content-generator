c75d46d4a0c75cdfe69a9773dd8572da4be56e64
Nyakamba <omwegaenock@gmail.com>
2024-02-22 16:08:24 +0300

    3 days trial

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -90,7 +90,7 @@ const logout = asyncHandler(async (req, res) => {
 
 //Profile
 const userProfile = asyncHandler(async (req, res) => {
-  const id = req.user?._id;
+  const id = req.user?.id;
   const user = await User.findById(id).select("-password");
 
   if (user) {
