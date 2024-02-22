4f723135188016325eef72d77df035c5f029631f
Nyakamba <omwegaenock@gmail.com>
2024-02-22 10:11:09 +0300

    profile

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { register, login } = require("../controllers/usersController");
+const {
+  register,
+  login,
+  logout,
+  userProfile,
+} = require("../controllers/usersController");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/register", register);
-
 usersRouter.post("/login", login);
+usersRouter.post("/logout", logout);
+usersRouter.get("/profile", userProfile);
 
 module.exports = usersRouter;
