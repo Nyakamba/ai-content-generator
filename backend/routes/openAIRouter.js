da531ff0bb68b740246cd4545d5a6f128950e073
Nyakamba <omwegaenock@gmail.com>
2024-02-23 06:55:49 +0300

    check request limit

--- backend/routes/openAIRouter.js
+++ backend/routes/openAIRouter.js
@@ -2,9 +2,15 @@ const express = require("express");
 
 const isAuthenticated = require("../middlewares/isAuthenticated");
 const openAIController = require("../controllers/openAIController");
+const checkApiRquestLimit = require("../middlewares/checkApiRequestLimit");
 
 const openAIRouter = express.Router();
 
-openAIRouter.post("/generate-content", isAuthenticated, openAIController);
+openAIRouter.post(
+  "/generate-content",
+  isAuthenticated,
+  checkApiRquestLimit,
+  openAIController
+);
 
 module.exports = openAIRouter;
