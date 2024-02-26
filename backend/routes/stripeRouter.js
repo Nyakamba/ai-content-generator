354f128201a2ba2911f639bae939ea848e94fbb7
Nyakamba <omwegaenock@gmail.com>
2024-02-26 16:13:46 +0300

    calculating next billing date

--- backend/routes/stripeRouter.js
+++ backend/routes/stripeRouter.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const isAuthenticated = require("../middlewares/isAuthenticated");
-const handleStripePayment = require("../controllers/handleStripePayment");
+const {
+  handleStripePayment,
+  handleFreeSubscription,
+} = require("../controllers/handleStripePayment");
 
 const stripeRouter = express.Router();
 
 stripeRouter.post("/checkout", isAuthenticated, handleStripePayment);
+stripeRouter.post("/free-plan", isAuthenticated, handleFreeSubscription);
 
 module.exports = stripeRouter;
