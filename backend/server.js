3d43b2abc0b74844d108a4baa6f6b89c9486fcb4
Nyakamba <omwegaenock@gmail.com>
2024-02-26 22:15:36 +0300

    payment

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,92 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cron = require("node-cron");
 require("dotenv").config();
 const usersRouter = require("./routes/usersRouter");
 const openAIRouter = require("./routes/openAIRouter");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 const stripeRouter = require("./routes/stripeRouter");
+const User = require("./models/User");
 require("./utils/connectDB")();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//Cron for the trial period : run every single day
+cron.schedule("0 0 * * * *", async () => {
+  try {
+    //get the current date
+    const today = new Date();
+    await User.updateMany(
+      {
+        trialActive: true,
+        trialExpires: { $lt: today },
+      },
+      {
+        trialActive: false,
+        subscriptionPlan: "Free",
+        monthlyRequestCount: 5,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+//Cron for free  plan : run at ht end of of every month
+cron.schedule("0 0 1 * * *", async () => {
+  try {
+    //get the current date
+    const today = new Date();
+    await User.updateMany(
+      {
+        subscriptionPlan: "Free",
+        nextBillingDate: { $lt: today },
+      },
+      {
+        monthlyRequestCount: 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//Cron for basic  plan : run at ht end of of every month
+cron.schedule("0 0 1 * * *", async () => {
+  try {
+    //get the current date
+    const today = new Date();
+    await User.updateMany(
+      {
+        subscriptionPlan: "Basic",
+        nextBillingDate: { $lt: today },
+      },
+      {
+        monthlyRequestCount: 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+//Cron for premium  plan : run at ht end of of every month
+cron.schedule("0 0 1 * * *", async () => {
+  try {
+    //get the current date
+    const today = new Date();
+    await User.updateMany(
+      {
+        subscriptionPlan: "Premium",
+        nextBillingDate: { $lt: today },
+      },
+      {
+        monthlyRequestCount: 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
 //----middleware----
 app.use(express.json()); //pas incoming json data
 app.use(cookieParser()); //pass the cookie automaticallly
