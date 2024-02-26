2eac7d70f15ce9b1138ea5bb00377d9adbc43d50
Nyakamba <omwegaenock@gmail.com>
2024-02-26 15:42:51 +0300

    payment integration

--- backend/controllers/handleStripePayment.js
+++ backend/controllers/handleStripePayment.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const stripe = require("stripe");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 //--------Stripe payment-------
 
@@ -7,6 +7,30 @@ const handleStripePayment = asyncHandler(async (req, res) => {
   const { amount, subscriptionPlan } = req.body;
   //get the user
   const user = req?.user;
+  //console.log(user);
   try {
-  } catch (error) {}
+    //create payment intent
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Number(amount) * 100,
+      currency: "usd",
+      //add some data to the meta object
+      metadata: {
+        userId: user?._id?.toString(),
+        userEmail: user?.email,
+        subscriptionPlan,
+      },
+    });
+
+    //send the response
+    res.json({
+      clientSecret: paymentIntent?.client_secret,
+      paymentId: paymentIntent?.id,
+      metadata: paymentIntent?.metadata,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 });
+
+module.exports = handleStripePayment;
