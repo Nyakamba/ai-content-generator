561c61b982226c4bd7361dd728e60e6bc1b9abfa
Nyakamba <omwegaenock@gmail.com>
2024-02-27 10:59:17 +0300

    user registration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cron = require("node-cron");
+const cors = require("cors");
 require("dotenv").config();
 const usersRouter = require("./routes/usersRouter");
 const openAIRouter = require("./routes/openAIRouter");
@@ -90,6 +91,11 @@ cron.schedule("0 0 1 * * *", async () => {
 //----middleware----
 app.use(express.json()); //pas incoming json data
 app.use(cookieParser()); //pass the cookie automaticallly
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 //-----Routes-----
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/openai", openAIRouter);
