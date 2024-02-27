6e615712ba296e7d713831e92cbbce395896362c
Nyakamba <omwegaenock@gmail.com>
2024-02-27 14:53:52 +0300

    auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App";
+import { AuthProvider } from "./AuthContext/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //React query client
@@ -11,7 +12,9 @@ const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
