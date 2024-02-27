e9f670a4e88daa116b5ca838bccb0c0dc74addc3
Nyakamba <omwegaenock@gmail.com>
2024-02-27 14:53:48 +0300

    auth context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./components/Users/Register";
+import Login from "./components/Users/Login";
+import Dashboard from "./components/Users/Dashboard";
+import PrivateNavbar from "./components/Navbar/PrivateNavbar";
+import PublicNavbar from "./components/Navbar/PublicNavbar";
+import Home from "./components/Home/Home";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        {/* Navbar */}
+        <PrivateNavbar />
         <Routes>
           <Route path="/register" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
