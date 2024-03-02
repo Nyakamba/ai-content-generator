c785a608776201472421fd37215d837883aea00e
Nyakamba <omwegaenock@gmail.com>
2024-03-02 22:26:53 +0300

    auth

--- frontend/src/components/AuthRoute/AuthRoute.js
+++ frontend/src/components/AuthRoute/AuthRoute.js
@@ -1,12 +1,11 @@
-import React from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { useLocation, Navigate } from "react-router-dom";
 import { useAuth } from "../../AuthContext/AuthContext";
 import AuthCheckingComponent from "../Alert/AuthCheckingComponent";
 
 const AuthRoute = ({ children }) => {
   const location = useLocation();
+  const { isAuthenticated, isError, isLoading } = useAuth();
 
-  const { isAuthenticated, isError, isLoading } = useAuth;
   if (isLoading) {
     return <AuthCheckingComponent />;
   }
