9ad0b1681d48c96c5346414424c9a40c361b84c6
Nyakamba <omwegaenock@gmail.com>
2024-03-02 19:02:04 +0300

    protecting components

--- frontend/src/components/AuthRoute/AuthRoute.js
+++ frontend/src/components/AuthRoute/AuthRoute.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../AuthContext/AuthContext";
+import AuthCheckingComponent from "../Alert/AuthCheckingComponent";
 
-const AuthRoute = () => {
+const AuthRoute = ({ children }) => {
+  const location = useLocation();
+
   const { isAuthenticated, isError, isLoading } = useAuth;
-  return <div></div>;
+  if (isLoading) {
+    return <AuthCheckingComponent />;
+  }
+  if (isError || isAuthenticated === false) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
 };
 
 export default AuthRoute;
