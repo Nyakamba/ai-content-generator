e971881e4a625f997d5ece9b196381dec4e15624
Nyakamba <omwegaenock@gmail.com>
2024-03-03 08:44:53 +0300

    dashport details fro the backend

--- frontend/src/apis/user/usersAPI.js
+++ frontend/src/apis/user/usersAPI.js
@@ -53,3 +53,15 @@ export const logoutAPI = async () => {
   );
   return response?.data;
 };
+
+//user profile
+export const getUserProfileApi = async () => {
+  const response = await axios.get(
+    "http://localhost:5000/api/v1/users/profile",
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+  return response?.data;
+};
