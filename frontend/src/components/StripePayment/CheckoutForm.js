27487aa1aa8e15b4f00ba65cd59a5dab1140d25f
Nyakamba <omwegaenock@gmail.com>
2024-03-04 17:40:37 +0300

    implementing payment in the frontend

---
+++ frontend/src/components/StripePayment/CheckoutForm.js
@@ -0,0 +1,72 @@
+import { useParams, useSearchParams } from "react-router-dom";
+import {
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { useState } from "react";
+
+const CheckoutForm = () => {
+  //Get the payload
+  const paramas = useParams();
+
+  const [searchParams] = useSearchParams();
+  const plan = paramas.plan;
+  const amount = searchParams.get("amount");
+  //stripe configuration
+  const stripe = useStripe();
+  const elements = useElements();
+  const [errorMessage, setErrorMessage] = useState(null);
+  console.log(amount);
+
+  //handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (elements === null) return;
+    const { error: submitError } = await elements.submit();
+    if (submitError) return;
+
+    try {
+      //prepare data for payment
+      const data = {
+        amount,
+        plan,
+      };
+      //make the http request
+      const { error } = await stripe.confirmPayment({
+        elements,
+        clientSecret:
+          "pi_3OqcaLGNfZDSdZAl0NFTXvTt_secret_AWEwPuLBmsIypQbEyP1pJhiQ8",
+        confirmParams: {
+          return_url: "http://localhost:3000/success",
+        },
+      });
+      if (error) {
+        setErrorMessage(error?.message);
+      }
+    } catch (error) {
+      setErrorMessage(error?.message);
+    }
+  };
+
+  return (
+    <div className="bg-gray-900 h-screen -mt-4 flex justify-center items-center  ">
+      <form
+        onSubmit={handleSubmit}
+        className="w-96 mx-auto my-6 p-6 bg-white rounded-lg shadow-md"
+      >
+        <div className="mb-4">
+          <PaymentElement />
+        </div>
+        <button className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gradient-to-r from-purple-500 to-blue-500 hover:from-indigo-600 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Pay
+        </button>
+        {errorMessage && (
+          <diV className="text-red-500 mt-4">{errorMessage}</diV>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default CheckoutForm;
