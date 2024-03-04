daed796c62501fef15a4b5228f919d3475e93754
Nyakamba <omwegaenock@gmail.com>
2024-03-04 21:06:37 +0300

    stripe payment frontend implementation

--- frontend/src/apis/stripePayment/stripePayment.js
+++ frontend/src/apis/stripePayment/stripePayment.js
@@ -10,3 +10,30 @@ export const handleFreeSubscriptionAPI = async () => {
   );
   return response?.data;
 };
+
+//stripe payment intent
+export const createStripePaymentIntentAPI = async (payment) => {
+  const response = await axios.post(
+    "http://localhost:5000/api/v1/stripe/checkout",
+    {
+      amount: payment?.amount,
+      subscriptionPlan: payment?.plan,
+    },
+    {
+      withCredentials: true,
+    }
+  );
+  return response?.data;
+};
+
+//verify payment intent
+export const verifyPaymentAPI = async (paymentId) => {
+  const response = await axios.post(
+    `http://localhost:5000/api/v1/stripe/verify-payment/${paymentId}`,
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+  return response?.data;
+};
