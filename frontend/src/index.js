9f3f979af2e6aba93347894bb545958a6bcd73ac
Nyakamba <omwegaenock@gmail.com>
2024-03-04 17:40:21 +0300

    implementing payment in the frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./AuthContext/AuthContext";
+
+//Stripe configuration
+const stripePromise = loadStripe(
+  "pk_test_51Oo22QGNfZDSdZAlw5yDwfH53nGGWCxdMCrddhoqWPpWKgzwL9BlOTeBcHI3jw7Ezs2pR2MiN7okJx3sVd3iUlmj003ScjmAdD"
+);
+
+const options = {
+  mode: "payment",
+  currency: "usd",
+  amount: 1099,
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //React query client
@@ -13,7 +26,9 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <App />
+        <Elements stripe={stripePromise} options={options}>
+          <App />
+        </Elements>
       </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
