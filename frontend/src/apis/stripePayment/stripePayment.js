713d1b6bf5a008875cae0a7c7165c8fda1be129f
Nyakamba <omwegaenock@gmail.com>
2024-03-04 11:16:56 +0300

    stripe payment functionality frontend

---
+++ frontend/src/apis/stripePayment/stripePayment.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+//stripe payment
+export const handleFreeSbscription = async () => {
+  const response = await axios.post(
+    "http://localhost:5000/api/v1/stripe/free-plan",
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+  return response?.data;
+};
