8b8544c8ea69498ef523c2d1e8d1f248c27a81b0
Nyakamba <omwegaenock@gmail.com>
2024-03-04 21:06:48 +0300

    stripe payment frontend implementation

--- frontend/src/components/StripePayment/CheckoutForm.js
+++ frontend/src/components/StripePayment/CheckoutForm.js
@@ -5,6 +5,9 @@ import {
   useElements,
 } from "@stripe/react-stripe-js";
 import { useState } from "react";
+import { createStripePaymentIntentAPI } from "../../apis/stripePayment/stripePayment";
+import { useMutation } from "@tanstack/react-query";
+import StatusMessage from "../Alert/Statusmessage";
 
 const CheckoutForm = () => {
   //Get the payload
@@ -13,11 +16,13 @@ const CheckoutForm = () => {
   const [searchParams] = useSearchParams();
   const plan = paramas.plan;
   const amount = searchParams.get("amount");
+  const mutation = useMutation({
+    mutationFn: createStripePaymentIntentAPI,
+  });
   //stripe configuration
   const stripe = useStripe();
   const elements = useElements();
   const [errorMessage, setErrorMessage] = useState(null);
-  console.log(amount);
 
   //handle submit
   const handleSubmit = async (e) => {
@@ -32,17 +37,20 @@ const CheckoutForm = () => {
         amount,
         plan,
       };
+      mutation.mutate(data);
+
       //make the http request
-      const { error } = await stripe.confirmPayment({
-        elements,
-        clientSecret:
-          "pi_3OqcaLGNfZDSdZAl0NFTXvTt_secret_AWEwPuLBmsIypQbEyP1pJhiQ8",
-        confirmParams: {
-          return_url: "http://localhost:3000/success",
-        },
-      });
-      if (error) {
-        setErrorMessage(error?.message);
+      if (mutation?.isSuccess) {
+        const { error } = await stripe.confirmPayment({
+          elements,
+          clientSecret: mutation?.data?.clientSecret,
+          confirmParams: {
+            return_url: "http://localhost:3000/success",
+          },
+        });
+        if (error) {
+          setErrorMessage(error?.message);
+        }
       }
     } catch (error) {
       setErrorMessage(error?.message);
@@ -58,6 +66,21 @@ const CheckoutForm = () => {
         <div className="mb-4">
           <PaymentElement />
         </div>
+        {/* loading */}
+        {mutation?.isPending && (
+          <StatusMessage type="loading" message="Loading please wait..." />
+        )}
+        {/* success */}
+        {mutation?.isSuccess && (
+          <StatusMessage type="success" message="Payment is successful" />
+        )}
+        {/* error */}
+        {mutation?.isError && (
+          <StatusMessage
+            type="error"
+            message={mutation?.error?.response.error}
+          />
+        )}
         <button className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gradient-to-r from-purple-500 to-blue-500 hover:from-indigo-600 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Pay
         </button>
