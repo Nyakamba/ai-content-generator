9bbb9785b40ffdf942f2aad8f58f91e2c67477c0
Nyakamba <omwegaenock@gmail.com>
2024-03-05 17:15:25 +0300

    testing

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -13,7 +13,7 @@ const usersRouter = express.Router();
 usersRouter.post("/register", register);
 usersRouter.post("/login", login);
 usersRouter.post("/logout", logout);
-usersRouter.get("/profile", isAuthenticated, userProfile);
+usersRouter.get("/profile", userProfile);
 usersRouter.get("/auth/check", isAuthenticated, checkAuth);
 
 module.exports = usersRouter;
